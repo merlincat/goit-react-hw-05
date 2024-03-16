cbf6a983006b2c2c691252d0aa86338632f21e39
NataOz <merlincat.no@gmail.com>
2024-03-16 20:06:35 +0200

    add initial files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,45 +1,16 @@
 import { useState, useEffect } from 'react';
 
-import ContactForm from '../ContactForm/ContactForm';
-import SearchBox from '../SearchBox/SearchBox';
-import ContactList from '../ContactList/ContactList';
-
 import css from './App.module.css';
 
-import initialContacts from '../../contacts.json';
-
 const App = () => {
-  const [contacts, setContactList] = useState(() => {
-    const storedContacts = JSON.parse(localStorage.getItem('contacts'));
-    return storedContacts || initialContacts;
-  });
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-  const addContact = newContact => {
-    setContactList(prevContacts => [...prevContacts, newContact]);
-  };
-  const deleteContact = contactId => {
-    setContactList(prevContacts => {
-      return prevContacts.filter(contact => contact.id !== contactId);
-    });
-  };
-
-  const [searchBarFilter, setFilter] = useState('');
-
-  const visibleContcats = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(searchBarFilter.toLowerCase())
-  );
-
-  return (
-    <div className={css.container}>
-      <h1>Phonebook</h1>
-      <ContactForm onAddContact={addContact} />
-      <SearchBox value={searchBarFilter} onSearch={setFilter} />
-      <ContactList visibleContcats={visibleContcats} onDelete={deleteContact} />
-    </div>
-  );
+  return <div className={css.container}></div>;
 };
 
 export default App;
+
+// 0015d55fbf52ddbb380bf794d1d9f16d
+
+// токен
+// eyJhbGciOiJIUzI1NiJ9
+//   .eyJhdWQiOiIwMDE1ZDU1ZmJmNTJkZGJiMzgwYmY3OTRkMWQ5ZjE2ZCIsInN1YiI6IjY1ZjVkYmFhZDRkNTA5MDE2NGFiNGJhYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ
+//   .JLcjF8iHOjbCLIG4N99UwwNYG - BrarS14qIyT4eAAw8;
